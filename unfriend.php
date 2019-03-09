3bb0da086924b3c2e6b7bfcddc303e98e263cce5
KaFanxdreon <48389303+KaFanxdreon@users.noreply.github.com>
2019-03-10 02:15:40 +0700

    Update unfriend.php

--- unfriend.php
+++ unfriend.php
@@ -1,23 +1,57 @@
-# Membuat list kosong untuk menampung hobi
-hobi = []
-stop = False
-i = 0
+<?php
+set_time_limit(0);
+require_once('color.php');
 
-# Mengisi hobi
-while(not stop):
-    hobi_baru = raw_input("Inputkan hobi yang ke-{}: ".format(i))
-    hobi.append(hobi_baru)
+function friendlist($token){
+	$a = json_decode(file_get_contents('https://graph.facebook.com/me/friends?access_token='.$token), true);
+	return $a['data'];
+}
+function last_active($id, $tok){
+	$a = json_decode(file_get_contents('https://graph.facebook.com/'.$id.'/feed?access_token='.$tok.'&limit=1'), true);
+	$date = $a['data'][0]['created_time'];
+	$aa = strtotime($date);
+	return date('Y', $aa);
+}
 
-    # Increment i
-    i += 1
-    
-    tanya = raw_input("Mau isi lagi? (y/t): ")
-    if(tanya == "t"): 
-        stop = True
+function unfriend($id, $token){
+	$url = 'https://graph.facebook.com/me/friends?uid='.$id.'&access_token='.$token;
+	$ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $result = curl_exec($ch);
+    curl_close($ch);
+	if($result == true){
+		$unf = Console::Yellow('[Gua Apus]');
+	} else {
+		$unf = Console::red('[Mikir lagi]');
+	}
+	return $unf;
+}
+echo Console::blue("     =========================\n");
+echo Console::blue("     Ada Salam Dari Ka Fan\n");
+echo Console::blue("     Pepel Negara +62 Tetap Edge\n");
+echo Console::blue("     =========================\n");
+echo Console::blue("     Facebook Auto Unfriend\n");
+echo Console::blue("        Inactive Users\n\n");
 
+//INPUT
+echo "Facebook token : ";
+$fbtoken = trim(fgets(STDIN));
+echo "Year : ";
+$year = trim(fgets(STDIN));
+echo "\n";
 
-# Cetak Semua Hobi
-print "=" * 10 
-print "Kamu memiliki {} hobi".format(len(hobi))
-for hb in hobi:
-    print "- {}".format(hb)
+$FL = friendlist($fbtoken);
+foreach($FL as $list){
+	$name = $list['name'];
+	$id = $list['id'];
+	$date = last_active($id, $fbtoken);
+	if($date < $year){
+		echo Console::red('[Tewas]').' '.$name.' ~ '.$date.' '.unfriend($id, $fbtoken);
+		echo "\r\n";
+	}else{
+		echo Console::green('[Aktif]').' '.$name.' ~ '.$date;
+		echo "\r\n";
+	}
+}
